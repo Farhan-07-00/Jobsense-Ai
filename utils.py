7233c3c1719ee6569c654ac55dfcdb3d051225a9
Farhan Akthar <fakthar930@gmail.com>
2026-04-16 12:43:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 import nltk
 
-# Download only if missing
+# Download NLTK resources if missing
 try:
     nltk.data.find("tokenizers/punkt")
 except LookupError:
     nltk.download("punkt")
+
+try:
+    nltk.data.find("tokenizers/punkt_tab")
+except LookupError:
+    nltk.download("punkt_tab")
 
 try:
     nltk.data.find("corpora/stopwords")
